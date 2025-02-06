7a207de79831bab704b9fecb75b673b354d42c2d
Mario Faria <mariofaria9@hotmail.com>
2025-02-05 23:22:05 -0300

    Atualizacao e remocao

--- app.js
+++ app.js
@@ -2,34 +2,55 @@
 
 let listaAmigos = [];
 
-function adicionarAmigo(){
+function adicionarAmigo() {
     let nome = document.querySelector('input').value;
     if (nome === '') {
         alert("Por favor, insira um nome!");
+        return;
     }
-    if(listaAmigos.includes(nome, [0])){
+    if (listaAmigos.includes(nome)) {
         alert("Este nome já existe, favor inserir outro amigo.");
-    } else{
+    } else {
         listaAmigos.push(nome);
-        exibirNome(nome);
+        exibirLista();
     }
     limparCampo();
 }
 
-function limparCampo(){
+function limparCampo() {
     let nome = document.querySelector('input');
-    nome.value = ' ';
+    nome.value = '';
 }
 
-function exibirNome(nome) {
+function exibirLista() {
     let lista = document.getElementById('listaAmigos');
-    let item = document.createElement('li');
-    item.textContent = nome;
-    lista.appendChild(item);
+    lista.innerHTML = ''; // Limpa a lista exibida antes de atualizar
+    listaAmigos.forEach(function(nome) {
+        let item = document.createElement('li');
+        item.textContent = nome;
+        lista.appendChild(item);
+    });
 }
 
 function apagarLista() {
     listaAmigos = []; // Limpa o array
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = ''; // Remove os elementos da lista do HTML
+}
+
+function limparCampoRemocao() {
+    let nomeParaApagar = document.getElementById('inputNomeApagar');
+    nomeParaApagar.value = '';
+}
+
+function apagarNome() {
+    let nomeParaApagar = document.getElementById('inputNomeApagar').value;
+    let index = listaAmigos.indexOf(nomeParaApagar);
+    if (index !== -1) {
+        listaAmigos.splice(index, 1); // Remove o nome da lista
+        exibirLista(); // Atualiza a lista exibida
+        limparCampoRemocao();
+    } else {
+        alert("Nome não encontrado na lista.");
+    }
 }
